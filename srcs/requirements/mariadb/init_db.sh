45cef06a9a5196fd9f39a7414bbee42ad8da0d32
TeteDeCactus <o.labrecque.lacasse@gmail.com>
2024-04-21 19:39:19 -0400

    ++

--- srcs/requirements/mariadb/init_db.sh
+++ srcs/requirements/mariadb/init_db.sh
@@ -1,22 +1,11 @@
 #!/bin/bash
 
-# Attendre que MySQL soit prêt
-# until mysqladmin ping -h localhost --silent; do
-#     echo 'En attente du démarrage de MySQL...'
-#     sleep 1
-# done
-
-# Créer la base de données et l'utilisateur
-# mysql -e "CREATE DATABASE IF NOT EXISTS ${SQL_DATABASE};" && \
-# mysql -e "CREATE USER IF NOT EXISTS '${SQL_USER}'@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';" && \
-# mysql -e "GRANT ALL PRIVILEGES ON ${SQL_DATABASE}.* TO '${SQL_USER}'@'localhost';"
-
-
-
 set -e  # Exit the script on any error
 
 # Start MariaDB service
 service mysql start
+
+mysql & 
 
 # Create database (use environment variable)
 mysql -u root -p"$SQL_ROOT_PASSWORD" -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
@@ -31,3 +20,5 @@ mysql -u root -p"$SQL_ROOT_PASSWORD" -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABAS
 mysql -u root -p"$SQL_ROOT_PASSWORD" -e "FLUSH PRIVILEGES;"
 
 echo "Database '${SQL_DATABASE}' created with user '${SQL_USER}' and password '${SQL_PASSWORD}'"
+
+bash
